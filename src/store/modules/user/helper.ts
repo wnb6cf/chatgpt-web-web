bad4fca9be0ae3f2f7315656d92009fa449741e3
kimi <109884135+wnb6cf@users.noreply.github.com>
2023-03-08 22:10:02 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'user',
+      description: 'Go to <a href="https://chat.readon.ml" class="text-blue-500" target="_blank" >API版本</a>',
     },
   }
 }
