154f6b7356be99b06f18eff7b3552719cba2fd23
kimi <109884135+wnb6cf@users.noreply.github.com>
2023-03-26 17:05:39 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -39,18 +39,18 @@ onMounted(() => {
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
+          此项目公布于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/binjie09/chatgpt-web"
+            href="https://readon.ml"
             target="_blank"
           >
-            Github
+            chatgpt
           </a>
           ，免费且基于 MIT 协议，没有任何形式的付费行为！
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
+          仅供学习交流使用！
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
